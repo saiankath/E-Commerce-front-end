7a9f93ebf60df285f55bd613674b10ed280510cf
saiankath <mesaiankath@gmail.com>
2020-08-13 15:37:35 -0300

    hi

---
+++ src/core/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Styles from "../styles.css"
+import {API} from "../backend"
+import Base from './Base';
+
+function Home() {
+    console.log("API IS", API);
+    return (
+        <Base title="Home page">
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">Test</button>
+                </div>
+            </div>
+        </Base>
+    )
+}
+
+export default Home
